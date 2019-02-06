5914f37fc4b27836f3e472bb2c54df35a91c0f10
ilabstaff <ilabstaff@172.16.116.82>
2019-02-05 17:20:39 -0800

    2/5/19

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,21 +11,56 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "November 15th";
+		String dadsBirthday = "?";
+		String myBirthday = "July 14th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String Bday = JOptionPane.showInputDialog("who's birthday do you want"); 
 		// 3. Print out what the user typed
-		
+		System.out.println(Bday);
 		// 4. if user asked for "mom"
 			//print mom's birthday
+		if (Bday.equals("mom")) {
+			JOptionPane.showMessageDialog(null, "November 15th");
+		}
+		
+		
 		// 5. if user asked for "dad"
 			// print dad's birthday
+		else if (Bday.equals("dad")) {
+			JOptionPane.showMessageDialog(null, "?");
+		}
 		// 6. if user asked for your name
 			// print myBirthday
+		else if (Bday.equals("my")) {
+			JOptionPane.showMessageDialog(null, "July 14th");
+		}
+		
+		
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		else { JOptionPane.showMessageDialog(null, "Sorry i dont know that persons birthday");
+			
+		}
 	} 
+
+	
+	
+	
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,27 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
-		// 2. Convert their answer into an int
-
+         String Kat = JOptionPane.showInputDialog("how many cats do you have");
+ 		// 2. Convert their answer into an int
+int kit = Integer.parseInt(Kat);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if (kit>=3) {
+JOptionPane.showMessageDialog(null, "your a crazy kat lady");	
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+else if (kit<3 && kit>0) {
+	playVideo("https://www.youtube.com/watch?v=oZFAcp-Qfbs");
+}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+else if (kit==0) {
+	playVideo("https://www.youtube.com/watch?v=_YIcdbigmco");
+}
 	}
 
 	static void playVideo(String videoURL) {
