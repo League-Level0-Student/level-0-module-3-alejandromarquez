e68d795908d2f120605060514e24cd0debf57e3e
ilabstaff <ilabstaff@172.16.116.79>
2019-03-19 17:36:11 -0700

    3/19/19 horoscope

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -12,17 +12,25 @@ public class BananaQuiz
 		//1. ask the user if they like bananas 
 		String ban = JOptionPane.showInputDialog("do you like bananas");
 		//2. if they say no, 
-		if (ban.equals) {
-			
+		if (ban.equals("no") ) {
+			JOptionPane.showMessageDialog(null, "your CRAZY!!!!!!!!!");
 		}
 			//tell them they are crazy 
 			//and end quiz
 		//3. if -they say yes 
+		else if (ban.equals("yes")) {
+		String nana = JOptionPane.showInputDialog("whats your favorite hobby");
+		JOptionPane.showMessageDialog(null, "bananas go good with "+ nana);
+		}
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
 		//4. OPTIONAL: if they say something other than “yes�? or “no�?
 		//	show a pop up that says “You are bananas!�?
+		
+		else {
+			JOptionPane.showMessageDialog(null, "You are bananas!�?");
+		}	
 	
 	}
 
---
+++ src/horoscope.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class horoscope {
+public static void main(String[] args) {
+	 JOptionPane.showInputDialog("whats your zodiac sign");
+	
+}
+}
