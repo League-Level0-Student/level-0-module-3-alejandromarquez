637c4135c131978925aee21b0a15abcea9bc9da2
ilabstaff <ilabstaff@172.16.116.69>
2019-05-20 17:51:45 -0700

    5/20/19 mon 5:51pm

--- src/horoscope.java
+++ src/horoscope.java
@@ -2,7 +2,14 @@ import javax.swing.JOptionPane;
 
 public class horoscope {
 public static void main(String[] args) {
-	 JOptionPane.showInputDialog("whats your zodiac sign");
-	
+	String zodiac =  JOptionPane.showInputDialog("whats your zodiac sign");
+	if (zodiac.equals("Cancer")) {
+	 JOptionPane.showMessageDialog(null, "When you have to solve a situation in an alien way to you today, you might learn something new.");
+	}
+	else if(zodiac.equals("scorpio")) {;
+	JOptionPane.showMessageDialog(null, "When something interrupts your routine or schedule today relax into it and be flexible, dont panic");
+	}
+	else if(zodiac.equals("Your open energy today means youll see possibilities and you are optimistic, but realty bites later."));
 }
+
 }
